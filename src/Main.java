5954f702e5d96f34ce673cab79777064eff227ee
edwardoboh <obohedward@gmail.com>
2023-11-22 20:52:00 +0100

    feat: method 5

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Main thread start");
-       Thread thread = new Thread(
-               () -> {
-                   System.out.println("Inner Thread Started");
-                   try{
-                       Thread.sleep(4000);
-                   }catch(Exception e){
-                       System.out.println(e.getMessage());
-                   }
-                   System.out.println("Inner Thread Ends");
-               }
-       );
-       thread.setDaemon(true);  // Use this line if you want the second thread to end immediately the main thread exits
+        Runnable task = () -> {
+            System.out.println("Child Thread");
+        };
+       Thread thread = new Thread(task);
+//       thread.setDaemon(true);  // Use this line if you want the second thread to end immediately the main thread exits
        thread.start();
 
        System.out.println("Main Thread Ends");
