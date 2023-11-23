07d1ea1f49fedec28d1a514e9088595dd426f6e8
edwardoboh <obohedward@gmail.com>
2023-11-23 07:54:44 +0100

    feat: exit a running thread gracefully

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,36 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Main thread start");
-        Runnable task = () -> {
-            System.out.println("Child Thread");
-        };
-       Thread thread = new Thread(task);
-//       thread.setDaemon(true);  // Use this line if you want the second thread to end immediately the main thread exits
-       thread.start();
-
-       System.out.println("Main Thread Ends");
+    public static void main(String[] args) throws Exception {
+        MyThread myThread = new MyThread();
+        Thread newThread = new Thread(myThread, "New Thread");
+        newThread.start();
+        Thread.sleep(5000);
+        myThread.requestExit(true);
     }
 
+    private static class MyThread implements Runnable {
+        private boolean threadState;
+
+        public void MyThread(){
+            threadState = false;
+        }
+        public void requestExit(boolean state){
+            threadState = !!state;
+        }
+        private void runThread(){
+            while(!threadState){
+                System.out.println("...");
+                try{
+                    Thread.sleep(2000);
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+        public void run(){
+            System.out.println("Begin Thread Execution");
+            runThread();
+            System.out.println("Exit Thread Execution");
+        }
+    }
 }
